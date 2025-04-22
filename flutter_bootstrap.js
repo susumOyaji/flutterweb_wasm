0b5046c5395868008bf0f2619daf8319d4119087
susumOyaji <susumOyaji@users.noreply.github.com>
2025-04-22 05:53:40 +0000

    deploy: 7c9b3476dca034f08d9ecbf2935c509ec4aa92e2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3900654815"
+    serviceWorkerVersion: "4037858740"
   }
 });
